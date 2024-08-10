eaa72814abdb9607ae4df6731e4abc060563d032
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2024-08-10 15:31:55 +0200

    Added user info. Removed call for coordinates

--- src/utils/users.js
+++ src/utils/users.js
@@ -2,28 +2,22 @@ import { API_URL, mockData } from "../../config"
 import users from '../assets/users.json'
 
 export const getUsers = () => {
-  if (mockData) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(users);
-      }, 1500);
-    })
-  }
+  return new Promise((resolve) => {
+    resolve(users);
+  })
+
   return fetch(`${API_URL}/users`)
     .then(response => response.json())
     .catch(err => console.log(err))
 }
 
 export const getUser = (id) => {
-  if (mockData) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log(users, id);
-        resolve(users.find(item => item.id == id));
-      }, 1500);
-    })
-  }
-  return fetch(`${API_URL}/users/${id}`)
+  return new Promise((resolve) => {
+    console.log(users, id);
+    resolve(users.find(item => item.id == id));
+  })
+
+    return fetch(`${API_URL}/users/${id}`)
     .then(response => response.json())
     .catch(err => console.log(err))
 }
