14766e87dec6007183d0855df4c34729d8ee2baa
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2024-08-10 14:53:50 +0200

    Updated design for map. Added reviews

--- src/components/RestaurantCardSkeleton.jsx
+++ src/components/RestaurantCardSkeleton.jsx
@@ -20,19 +20,4 @@ export const RestaurantCardSkeleton = ({ restaurant }) => {
       </div>
     </>
   );
-
-  return (
-    <>
-      <div className="flex w-52 flex-col gap-4">
-        <div className="flex items-center gap-4">
-          <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
-          <div className="flex flex-col gap-4">
-            <div className="skeleton h-4 w-20"></div>
-            <div className="skeleton h-4 w-28"></div>
-          </div>
-        </div>
-        <div className="skeleton h-32 w-full"></div>
-      </div>
-    </>
-  )
 }
