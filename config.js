0e8bc15e083cbf4d226e28748acd9647a9ec5407
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2024-08-10 15:27:47 +0200

    Fixed map for deploy

--- config.js
+++ config.js
@@ -1,2 +1,18 @@
 export const API_URL = 'https://mini-yelp-api.vercel.app/api';
 export const mockData = false;
+export const randomCoordinates = [{
+  lat: 42.345512,
+  lng: 5.1234124,
+}, {
+  lat: 102.314415,
+  lng: 30.1234124,
+}, {
+  lat: 78.345512,
+  lng: 21.1234124,
+}, {
+  lat: 20.345512,
+  lng: 35.1234124,
+}, {
+  lat: 120.345512,
+  lng: 67.1234124,
+}];
